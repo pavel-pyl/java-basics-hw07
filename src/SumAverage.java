e942c0beb66c9dba7efcf8999d366b6ec1b09576
pavel-pyl <4469540@gmail.com>
2022-02-07 23:28:12 +0200

    Home work

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -11,8 +11,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int lowerBound, int upperBound) {
-    // TODO fill in code here using for loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int i = lowerBound; i<= upperBound; i++) {
+      sum += i;
+    }
+    return sum;
   }
 
   /**
@@ -22,8 +25,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int[] numbers) {
-    // TODO fill in code here using for each loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int i: numbers) {
+      sum += i;
+    }
+    return sum;
   }
 
   /**
@@ -34,8 +40,13 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int lowerBound, int upperBound) {
-    // TODO fill in code here using while loop and replace the return statement
-    return -1;
+    double i = lowerBound;
+    int sum = 0;
+    while (i <= upperBound){
+      sum += i;
+      i++;
+    }
+    return sum / (i - lowerBound);
   }
 
   /**
@@ -45,7 +56,12 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int[] numbers) {
-    // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
+
+    double sum = 0;
+    int i = 0;
+    do {
+      sum += numbers[i];
+    } while (++i < numbers.length);
+    return  sum / numbers.length;
   }
 }
--- src/SumAverageTest.java
+++ src/SumAverageTest.java
@@ -15,10 +15,11 @@ public class SumAverageTest {
   @DataProvider(name = "Sum")
   public static Object[][] sum() {
     return new Object[][]{
-        {1, 100, 5050},
-        {-100, 100, 0},
-        {1, 1, 1}
-        // TODO add 2 more test data here
+            {1, 100, 5050},
+            {-100, 100, 0},
+            {1, 1, 1},
+            {-1, -1, -1},
+            {0, 0, 0}
     };
   }
 
@@ -30,10 +31,11 @@ public class SumAverageTest {
   @DataProvider(name = "Average")
   public static Object[][] average() {
     return new Object[][]{
-        {1, 100, 50.5},
-        {-100, 100, 0.0},
-        {1, 1, 1.0}
-        // TODO add 2 more test data here
+            {1, 100, 50.5},
+            {-100, 100, 0.0},
+            {1, 1, 1.0},
+            {-1, -1, -1.0},
+            {0, 0, 0}
     };
   }
 
